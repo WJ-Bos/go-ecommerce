699879ccbe0e05c248249324940c089bdfbc355e
WJbos <wjbosdev@gmail.com>
2025-04-11 12:15:27 +0200

    Created types for UserStore, RegisterPAyload and USer

--- types/types.go
+++ types/types.go
@@ -1,1 +1,25 @@
 package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type RegisterUserPayload struct {
+	Firstname string `json:"firstName"`
+	Lastname  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	Firstname string    `json:"firstName"`
+	Lastname  string    `json:"lastName"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
