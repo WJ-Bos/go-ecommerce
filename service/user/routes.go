edf9a52bb55b15a0e4cbe34511dd0bf379e6156f
WJbos <wjbosdev@gmail.com>
2025-04-11 12:15:50 +0200

    Implemented the actual register logic

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,15 +1,22 @@
 package user
 
 import (
+	"Ecom/service/auth"
+	"Ecom/types"
+	"Ecom/utils"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{
+		store: store,
+	}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -17,10 +24,39 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
-func (h *Handler) handleLogin(http.ResponseWriter, *http.Request) {
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *Handler) handleRegister(http.ResponseWriter, *http.Request) {
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get the json payload from the request
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{
+		Firstname: payload.Firstname,
+		Lastname:  payload.Lastname,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	utils.WriteJson(w, http.StatusCreated, nil)
 }
